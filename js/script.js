5cf49f174d0e4c0c589d1de7f9e1fb7932d9132e
legacy <legacywebhub.@gmail.com>
2022-04-19 11:48:38 +0100

    Javascript code restructure commit

--- js/script.js
+++ js/script.js
@@ -6,15 +6,13 @@ const popular = document.getElementById("popular"),
     menu = document.getElementById("menu"),
     sideBar = document.getElementById("side-bar"),
     closeIcon = document.getElementById("close"),
-    weatherSection = document.getElementById("section-2"),
-    weatherData = document.getElementById("weather-details"),
+    weatherContainer = document.getElementById("weather-details"),
     message = document.getElementById("message"),
-    iconContainer = document.getElementById("weather-icon-container");
-    name = document.getElementById("city-name"),
+    iconContainer = document.getElementById("weather-icon-container"),
     weather = document.getElementById("weather"),
-    temp = document.getElementById("temperature"),
-    press = document.getElementById("pressure"),
-    hum = document.getElementById("humidity"),
+    temperature = document.getElementById("temperature"),
+    pressure = document.getElementById("pressure"),
+    humidy = document.getElementById("humidity"),
     form = document.forms["my-form"],
     input = form["city"];
 let index = 0;
@@ -26,74 +24,24 @@ closeIcon.addEventListener('click', closeSideBar);
 popular.addEventListener('click', ()=>{popularList.classList.toggle("display-sub-list");});
 other.addEventListener('click', ()=>{otherList.classList.toggle("display-sub-list");});
 
-
-
-
 //Functions
+//function to show sidebar
 function showSideBar() {
     sideBar.style.right = "0rem";
+    /* checks if sidebar is opened and closes the sidebar 
+    when any part of the page outside the sidebar is clicked */
     if (sideBar.style.right == "0rem") {
         main = document.querySelector("main");
         main.addEventListener('click', closeSideBar);
     }
 };
 
-
+//function to close sidebar
 function closeSideBar() {
-    sideBar.style.right = "-1000rem";
+    sideBar.style.right = "-100rem";
 };
 
-
-async function searchWeather(e, location) {
-    e.preventDefault();
-
-    setInterval(fetchingData, 100);
-
-    setTimeout(ajaxCall, 5000);
-}
-
-async function ajaxCall(){
-    const city = input.value || location;
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=01496641e8e04d3c4d8473dba298674b`;
-    const ourRequest = await fetch(url);
-    const data = ourRequest.json();
-
-    console.log(data)
-
-    // processWeather();
-    message.style.display = "none";
-
-    t = data.main.temp - 273;
-    p = data.main.pressure / 1000;
-    h = data.main.humidity;
-    w = data.weather[0].description;
-    console.log(data.weather[0].main);
-    
-    temp.innerText = Math.round(t);
-    press.innerText = Math.round(p);
-    hum.innerText = Math.round(h);
-    weather.innerText = w;
-    name.innerText = data.name.toUpperCase(); 
-
-    if (data.weather[0].main == "Sun") {
-        weatherIcon = document.createElement('i');
-        weatherIcon.classList.add("fas fa-cloud-sun fa-3x");
-        iconContainer.appendChild(weatherIcon);
-        // weatherIcon = `<i class="fas fa-sun fa-3x"></i>`;
-    } else if (data.weather[0].main == "Rain") {
-        weatherIcon = document.createElement('i');
-        weatherIcon.classList.add("fas fa-cloud-rain fa-3x");
-        iconContainer.appendChild(weatherIcon);
-        // weatherIcon = `<i class="fas fa-cloud-rain fa-3x"></i>`;
-    } else if (data.weather[0].main == "Cloud") {
-        weatherIcon = document.createElement('i');
-        weatherIcon.classList.add("fas fa-cloud fa-3x");
-        iconContainer.appendChild(weatherIcon);
-        // weatherIcon = `<i class=\"fas fa-cloud fa-3x\"></i>`;
-    }
-    weatherData.style.display = "inline-block";
-}
-
+//function to display animation text while fetching data
 function fetchingData() {
     message.textContent = "Fetching Data...";
     message.innerText = message.innerText.slice(0, index);
@@ -103,6 +51,32 @@ function fetchingData() {
     }
 }
 
-// function processWeather() {
+async function searchWeather(e, location) {
+    e.preventDefault();
+    setInterval(fetchingData(), 2000);
 
-// }
+    const city = input.value || location;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=01496641e8e04d3c4d8473dba298674b`;
+           
+    const ourRequest = await fetch(url);
+    const data = await ourRequest.json();
+    console.log(data);
+
+    // updating the Dom 
+    temperature.innerText = Math.round(data.main.temp - 273);
+    pressure.innerText = Math.round(data.main.pressure);
+    humidy.innerText = Math.round(data.main.humidity);
+    weather.innerText = data.weather[0].description;
+    message.innerText = data.name.toUpperCase(); 
+
+    //determining weather icon to display in the DOM
+    if (data.weather[0].main == "Sun") {
+        iconContainer.innerHTML = '<i class="fas fa-cloud-sun fa-3x"></i>';
+    } else if (data.weather[0].main == "Rain") {
+        iconContainer.innerHTML = '<i class="fas fa-cloud-rain fa-3x"></i>';
+    } else if (data.weather[0].main == "Clouds") {
+        iconContainer.innerHTML = '<i class="fas fa-cloud fa-3x"></i>';
+    }
+    weatherContainer.style.display = "contents";
+    input.value = '';
+}
