9f62dcb9bdefba20314f5bc2b34b32223e7f991c
legacy <legacywebhub.@gmail.com>
2022-04-22 09:34:15 +0100

    Project functional commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 //Variables declarartions
-const popular = document.getElementById("popular"),
+const popularLocations = document.getElementsByClassName('popular'),
+    popular = document.getElementById("popular"),
     popularList = document.getElementById("popular-list"),
     other = document.getElementById("other-apps"),
     otherList = document.getElementById("other-list"),
@@ -13,18 +14,34 @@ const popular = document.getElementById("popular"),
     temperature = document.getElementById("temperature"),
     pressure = document.getElementById("pressure"),
     humidy = document.getElementById("humidity"),
+    country = document.getElementById("country"),
     form = document.forms["my-form"],
-    input = form["city"];
-let index = 0;
+    searchInput = form["city"];
+let i = 0,
+    txt = 'Loading...',
+    speed = 20;
+
+/* --------------- Code section --------------- */
+// checking if popular locations were clicked
+for (x = 0; x < popularLocations.length; x++) {
+    popularLocations[x].addEventListener('click', (e)=>{
+        let popularLocation = e.target.textContent.toLowerCase();
+        searchWeather(popularLocation);
+    })
+}
 
 //Event Listeners and onclicks
-form.addEventListener('submit', searchWeather);
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    searchWeather();
+});
 menu.addEventListener('click', showSideBar);
 closeIcon.addEventListener('click', closeSideBar);
 popular.addEventListener('click', ()=>{popularList.classList.toggle("display-sub-list");});
 other.addEventListener('click', ()=>{otherList.classList.toggle("display-sub-list");});
 
-//Functions
+
+/* --------------- Functions --------------- */
 //function to show sidebar
 function showSideBar() {
     sideBar.style.right = "0rem";
@@ -42,41 +59,49 @@ function closeSideBar() {
 };
 
 //function to display animation text while fetching data
-function fetchingData() {
-    message.textContent = "Fetching Data...";
-    message.innerText = message.innerText.slice(0, index);
-    index++;
-    if(index > message.length) {
-        index = 0;
+function loadAnimation() {
+    if (i < txt.length) {
+      message.innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(loadAnimation, speed);
     }
 }
 
-async function searchWeather(e, location) {
-    e.preventDefault();
-    setInterval(fetchingData(), 2000);
+// function to search weather and update the dom
+async function searchWeather(location) {
+    message.innerText = '';
 
-    const city = input.value || location;
+    loadAnimation();
+
+    const city = location || searchInput.value;
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=01496641e8e04d3c4d8473dba298674b`;
-           
+    
+    // fetching from API
     const ourRequest = await fetch(url);
+    // converting our response from string to json format
     const data = await ourRequest.json();
-    console.log(data);
 
-    // updating the Dom 
-    temperature.innerText = Math.round(data.main.temp - 273);
-    pressure.innerText = Math.round(data.main.pressure);
-    humidy.innerText = Math.round(data.main.humidity);
-    weather.innerText = data.weather[0].description;
-    message.innerText = data.name.toUpperCase(); 
-
-    //determining weather icon to display in the DOM
+    // determining weather icon to display in the DOM
     if (data.weather[0].main == "Sun") {
-        iconContainer.innerHTML = '<i class="fas fa-cloud-sun fa-3x"></i>';
+        iconContainer.innerHTML = '<i class="fas fa-sun-bright fa-3x"></i>';
     } else if (data.weather[0].main == "Rain") {
         iconContainer.innerHTML = '<i class="fas fa-cloud-rain fa-3x"></i>';
     } else if (data.weather[0].main == "Clouds") {
         iconContainer.innerHTML = '<i class="fas fa-cloud fa-3x"></i>';
+    } else if (data.weather[0].main == "Clear") {
+        iconContainer.innerHTML = '<i class="fa fa-sun fa-3x"></i>';
+    } else if (data.weather[0].main == "Haze") {
+        iconContainer.innerHTML = '<i class="fa-solid fa-sun-haze"></i>';
+    } else {
+        iconContainer.innerHTML = '';
     }
+    // updating the Dom 
+    temperature.innerText = Math.round(data.main.temp - 273);
+    pressure.innerText = data.main.pressure / 1000;
+    humidy.innerText = Math.round(data.main.humidity);
+    weather.innerText = data.weather[0].description;
+    message.innerText = data.name.toUpperCase();
+    country.innerText = data.sys.country.toUpperCase();
     weatherContainer.style.display = "contents";
-    input.value = '';
+    searchInput.value = '';
 }
