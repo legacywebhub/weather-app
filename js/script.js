06d810f447ad7452f1ba3e78179f07ba0e71ca22
legacy <legacywebhub.@gmail.com>
2022-04-23 05:58:17 +0100

    Upgrade commit
    Error checking has been implemented while fetching data, animation/DOM-update clash has been fixed, slight UI change

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ const popularLocations = document.getElementsByClassName('popular'),
     searchInput = form["city"];
 let i = 0,
     txt = 'Loading...',
-    speed = 20;
+    speed = 50;
 
 /* --------------- Code section --------------- */
 // checking if popular locations were clicked
@@ -69,39 +69,64 @@ function loadAnimation() {
 
 // function to search weather and update the dom
 async function searchWeather(location) {
-    message.innerText = '';
-
-    loadAnimation();
+    weatherContainer.style.display = "none";
 
     const city = location || searchInput.value;
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=01496641e8e04d3c4d8473dba298674b`;
-    
-    // fetching from API
-    const ourRequest = await fetch(url);
-    // converting our response from string to json format
-    const data = await ourRequest.json();
 
-    // determining weather icon to display in the DOM
-    if (data.weather[0].main == "Sun") {
-        iconContainer.innerHTML = '<i class="fas fa-sun-bright fa-3x"></i>';
-    } else if (data.weather[0].main == "Rain") {
-        iconContainer.innerHTML = '<i class="fas fa-cloud-rain fa-3x"></i>';
-    } else if (data.weather[0].main == "Clouds") {
-        iconContainer.innerHTML = '<i class="fas fa-cloud fa-3x"></i>';
-    } else if (data.weather[0].main == "Clear") {
-        iconContainer.innerHTML = '<i class="fa fa-sun fa-3x"></i>';
-    } else if (data.weather[0].main == "Haze") {
-        iconContainer.innerHTML = '<i class="fa-solid fa-sun-haze"></i>';
-    } else {
-        iconContainer.innerHTML = '';
+    //loading text animation
+    message.innerHTML =  '';
+    loadAnimation();
+
+    try {
+        // fetching from API
+        const ourRequest = await fetch(url);
+        // converting our response from string to json format
+        const data = await ourRequest.json();
+        console.log(data);
+
+        // determining weather icon to display in the DOM
+        if (data.weather[0].main == "Sun") {
+            iconContainer.innerHTML = '<i class="fas fa-sun fa-3x"></i>';
+        } else if (data.weather[0].main == "Rain") {
+            iconContainer.innerHTML = '<i class="fas fa-cloud-rain fa-3x"></i>';
+        } else if (data.weather[0].main == "Clouds") {
+            iconContainer.innerHTML = '<i class="fas fa-cloud fa-3x"></i>';
+        } else if (data.weather[0].main == "Clear") {
+            iconContainer.innerHTML = '<i class="fa fa-sun fa-3x"></i>';
+        } else if (data.weather[0].main == "Haze") {
+            iconContainer.innerHTML = '<img src="images/haze.png" width="130">';
+        } else if (data.weather[0].main == "Fog") {
+            iconContainer.innerHTML = '<img src="images/fog.png" width="100">';
+        } else if (data.weather[0].main == "Snow") {
+            iconContainer.innerHTML = '<img src="images/snow.png" width="100">';
+        } else if (data.weather[0].main == "Smoke") {
+            iconContainer.innerHTML = '<img src="images/smoke.png" width="100">';
+        } else {
+            iconContainer.innerHTML = '';
+        }
+
+        // updating the DOM data
+        temperature.innerText = Math.round(data.main.temp - 273);
+        pressure.innerText = data.main.pressure / 1000;
+        humidy.innerText = Math.round(data.main.humidity);
+        weather.innerText = data.weather[0].description;
+        country.innerText = data.sys.country.toUpperCase();
+
+        // waiting for our animation to finish before displaying the DOM
+        setTimeout(()=>{
+            message.innerHTML =  '';
+            message.innerText = data.name.toUpperCase();
+            weatherContainer.style.display = "contents";
+            searchInput.value = '';
+        }, 3000);
+    } catch (err) { // catching any error
+        setTimeout(()=>{
+        message.innerText =  '';
+        message.innerText = 'Sorry there was an error while fetching weather data';
+        weatherContainer.style.display = "none";
+        }, 10000);
     }
-    // updating the Dom 
-    temperature.innerText = Math.round(data.main.temp - 273);
-    pressure.innerText = data.main.pressure / 1000;
-    humidy.innerText = Math.round(data.main.humidity);
-    weather.innerText = data.weather[0].description;
-    message.innerText = data.name.toUpperCase();
-    country.innerText = data.sys.country.toUpperCase();
-    weatherContainer.style.display = "contents";
-    searchInput.value = '';
+    // resetting our animation variable
+    i = 0;
 }
